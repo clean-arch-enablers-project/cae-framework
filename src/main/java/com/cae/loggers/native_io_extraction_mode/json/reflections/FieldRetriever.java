beb76c7f0af4dc3c19e16d18bba9d9e85b4a19dd
Zé Lúcio Jr <joselucioalmeidajunior@gmail.com>
2024-05-08 21:48:20 -0300

    fix: getting fields from abstract super classes

--- src/main/java/com/cae/loggers/native_io_extraction_mode/json/reflections/FieldRetriever.java
+++ src/main/java/com/cae/loggers/native_io_extraction_mode/json/reflections/FieldRetriever.java
@@ -5,19 +5,30 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FieldRetriever {
 
     public static Field getField(String fieldName, Object object){
         try {
-            return object.getClass().getDeclaredField(fieldName);
+            var classWhereFieldIsDeclared = FieldRetriever.findRightClassFrom(object.getClass(), fieldName);
+            return classWhereFieldIsDeclared.getDeclaredField(fieldName);
         } catch (NoSuchFieldException e) {
             throw new InternalMappedException(
                     "Something went wrong while trying to retrieve the field " + fieldName,
                     "More details: " + e
             );
         }
+    }
+
+    private static Class<?> findRightClassFrom(Class<?> clazz, String fieldName) {
+        if (clazz == Object.class)
+            throw new InternalMappedException("Problem trying to extract I/O data", "Not possible to find the class where the field '" + fieldName + "' was declared");
+        var fields = clazz.getDeclaredFields();
+        if (Arrays.stream(fields).anyMatch(field -> field.getName().equals(fieldName)))
+            return clazz;
+        return FieldRetriever.findRightClassFrom(clazz.getSuperclass(), fieldName);
     }
 
 }
