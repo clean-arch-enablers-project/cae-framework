c87795c66a8d75efd4b352186d911f59445cf169
Zé Lúcio Jr <joselucioalmeidajunior@gmail.com>
2024-05-18 20:32:34 -0300

    feat: skipping not found fields

--- src/main/java/com/cae/loggers/native_io_extraction_mode/json/reflections/FieldRetriever.java
+++ src/main/java/com/cae/loggers/native_io_extraction_mode/json/reflections/FieldRetriever.java
@@ -6,29 +6,34 @@ import lombok.NoArgsConstructor;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Optional;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FieldRetriever {
 
-    public static Field getField(String fieldName, Object object){
-        try {
-            var classWhereFieldIsDeclared = FieldRetriever.findRightClassFrom(object.getClass(), fieldName);
-            return classWhereFieldIsDeclared.getDeclaredField(fieldName);
-        } catch (NoSuchFieldException e) {
-            throw new InternalMappedException(
-                    "Something went wrong while trying to retrieve the field " + fieldName,
-                    "More details: " + e
-            );
-        }
+    public static Optional<Field> getField(String fieldName, Object object){
+        var classWhereFieldIsDeclared = Optional.ofNullable(FieldRetriever.findRightClassFrom(object.getClass(), fieldName));
+        return classWhereFieldIsDeclared.map(foundClass -> FieldRetriever.retrieveField(foundClass, fieldName));
     }
 
     private static Class<?> findRightClassFrom(Class<?> clazz, String fieldName) {
         if (clazz == Object.class)
-            throw new InternalMappedException("Problem trying to extract I/O data", "Not possible to find the class where the field '" + fieldName + "' was declared");
+            return null;
         var fields = clazz.getDeclaredFields();
         if (Arrays.stream(fields).anyMatch(field -> field.getName().equals(fieldName)))
             return clazz;
         return FieldRetriever.findRightClassFrom(clazz.getSuperclass(), fieldName);
+    }
+
+    private static Field retrieveField(Class<?> foundClass, String fieldName){
+        try {
+            return foundClass.getDeclaredField(fieldName);
+        } catch (NoSuchFieldException e) {
+            throw new InternalMappedException(
+                    "Something went wrong while trying to retrieve the field '" + fieldName + "' in '" + foundClass.getSimpleName() + "'",
+                    "We found the class in which the field should be located, but at the retrievement attempt something went unexpectedly wrong. See the exception: " + e
+            );
+        }
     }
 
 }
